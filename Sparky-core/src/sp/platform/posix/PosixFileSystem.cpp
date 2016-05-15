7dc6d66f46b7c3cf52f1dd2b8f59d6de2e05e7f3
Aritz Lopez <aritzhack@gmail.com>
2016-05-15 11:45:27 +0200

    Implemented PosixFileSystem

--- Sparky-core/src/sp/platform/posix/PosixFileSystem.cpp
+++ Sparky-core/src/sp/platform/posix/PosixFileSystem.cpp
@@ -5,6 +5,7 @@
 
 #include <unistd.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 namespace sp {
 
@@ -15,21 +16,43 @@ namespace sp {
 
 	int64 FileSystem::GetFileSize(const String& path)
 	{
-		return 0;
+		struct stat st;
+		if (stat(path.c_str(), &st) == -1) return -1;
+		return (int64) st.st_size;
+	}
+
+	bool ReadFileInternal(const String& path, void* buffer, int64 size) {
+		int fd = open(path.c_str(), O_RDONLY);
+		if (fd == -1) return false;
+		if (size < 0) size = FileSystem::GetFileSize(path);
+		ssize_t result = read(fd, buffer, (size_t) size);
+		close(fd);
+		return result == size;
 	}
 
 	bool FileSystem::ReadFile(const String& path, void* buffer, int64 size)
 	{
-		return false;
+		return ReadFileInternal(path, buffer, size);
 	}
 
 	byte* FileSystem::ReadFile(const String& path)
 	{
-		return nullptr;
+		int64 size = FileSystem::GetFileSize(path);
+		if (size == -1) return nullptr;
+		byte* buffer = spnew byte[size];
+		return ReadFileInternal(path, buffer, size) ? buffer : nullptr;
 	}
 
 	String FileSystem::ReadTextFile(const String& path)
 	{
+		int64 size = FileSystem::GetFileSize(path);
+		String result(size, 0);
+		if (ReadFileInternal(path, &result[0], size))
+		{
+			// Strip carriage returns
+			result.erase(std::remove(result.begin(), result.end(), '\r'), result.end());
+			return result;
+		}
 		return String();
 	}
 
